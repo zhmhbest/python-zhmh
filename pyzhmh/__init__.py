013e236fb72aec514ea14bac2934d76e0ede01c3
zhmhbest <zhmhbest@gmail.com>
2020-08-09 00:48:45 +0800

    0.0.6

--- pyzhmh/__init__.py
+++ pyzhmh/__init__.py
@@ -1,4 +1,4 @@
 from .rich import RichPrint
 from .magic import __dirname__, __line__, __fun__, __assert__
 from .download import download_one_file, unpack_one_file
-__VERSION = '0.0.5'
+__VERSION = '0.0.6'
