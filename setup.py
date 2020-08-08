a63f72bb46df42e3a1583f5a2f2f679c2ae5b087
zhmhbest <zhmhbest@gmail.com>
2020-08-09 00:24:28 +0800

    recover setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+import setuptools
+from zhmh import __VERSION
+
+with open("README.md", "r") as fp:
+    long_description = fp.read()
+
+setuptools.setup(
+    # 环境依赖（版本、包、文件<包:匹配>）
+    # python_requires='>=3',
+    # install_requires=[''],
+    # package_data={'': ['*.txt']},
+    # 'MANIFEST.in': 打包时包含/排除指定规则的文件
+
+    # 打包配置
+    # scripts=['.py'],
+    packages=setuptools.find_packages(),  # exclude=['package']
+
+    # 作者资料
+    author="zhmh",
+    author_email="zhmhbest@gmail.com",
+
+    # 模块信息
+    name="python-zhmh",
+    version=__VERSION,
+    description="Python Common Command",
+    # keywords="hello common",
+    license="MIT",  # https://choosealicense.com/
+    url="https://github.com/zhmhbest/python-zhmh",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
